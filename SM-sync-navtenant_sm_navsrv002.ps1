6eca965ab1b5bcb86a207a57a9e14c7636636af6
porta76 <60570069+porta76@users.noreply.github.com>
2022-03-08 19:37:44 +0100

    Add files via upload

---
+++ SM-sync-navtenant_sm_navsrv002.ps1
@@ -0,0 +1,5 @@
+import-Module 'C:\Program Files\Microsoft Dynamics NAV\110\Service\NavAdminTool.ps1'
+Sync-NAVTenant -ServerInstance nav_kontor
+Sync-NAVTenant -ServerInstance nav_jobb
+Sync-NAVTenant -ServerInstance nav_dc
+Sync-NAVTenant -ServerInstance nav_lager
---
+++ SM-sync-navtenant_sm_sql003.ps1
@@ -0,0 +1,2 @@
+import-Module 'C:\Program Files\Microsoft Dynamics NAV\110\Service\NavAdminTool.ps1'
+Sync-NAVTenant -ServerInstance nav_kontor
